8f2239ac1144de7ed766555684e0ddf61d581a49
Bishwonath <112367927+Bishwonath@users.noreply.github.com>
2022-11-22 12:54:19 +0545

    Add files via upload

---
+++ example 1.py
@@ -0,0 +1,15 @@
+
+
+
+savings=int(input("enter the amount of money"))
+if savings>3000:
+    if savings>10000:
+        print("road trip")
+    else:
+        print("hiking")
+elif savings>2000:
+     print("burger house")
+else:
+    print("stay at home")
+
+    
